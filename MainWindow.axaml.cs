763be5a83b685c19dfdd7b72c5808f2b95f53bc2
sulorat <135011124+sulorat@users.noreply.github.com>
2024-09-23 16:35:00 +0300

    Add files via upload

---
+++ MainWindow.axaml.cs
@@ -0,0 +1,67 @@
+using Avalonia.Controls;
+using HarfBuzzSharp;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using static demo2.MainWindow;
+
+namespace demo2
+{
+    public class ClientPresenter : Client
+    {
+        public int ClientID { get => this.Id; }
+        public int ClientGender { get => this.Gendercode; }
+        public string ClientName { get => this.Firstname; }
+        public string ClientLastName { get => this.Lastname; }
+        public string ClientPatronymic { get => Patronymic; }
+        public DateTime? ClientBirthday { get => Birthday; }
+        public string ClientNumberPhone { get => Phone; }
+        public DateTime? ClientLastTimeOnline { get => Registrationdate; }
+        public string ClientEmail { get => Email; }
+        public ObservableCollection<Tags> ClientTags { get; set; }
+
+    }
+    public class Tags : Tag
+    {
+        public string TagTitle { get => this.Title; set => TagTitle = value; }
+        public string Color { get => this.Color; set => Color = value; }
+    }
+    public partial class MainWindow : Window
+    {
+       
+        private List<ClientPresenter> _clients= new List<ClientPresenter>();
+        private ObservableCollection<ClientPresenter>_displayClients= new ObservableCollection<ClientPresenter>();
+
+        private string[] SortValues = new string[] { "Фамилия","Последнее посещение","Количество посещений" };
+
+        private string[] FilterValues = new string[] { "Все", "Мужчина", "Женщина" };
+
+
+        public MainWindow()
+        {
+
+            InitializeComponent();
+
+            using(var dbContext =new ShaluhinContext())
+            {
+                _clients = dbContext.Clients.Select(clients => new ClientPresenter
+                {
+                    Firstname = clients.Firstname,
+                    Lastname = clients.Lastname,
+                    Patronymic = clients.Patronymic,
+                    Gendercode = clients.Gendercode,
+                    Birthday = clients.Birthday,
+                    Phone = clients.Phone,
+                    Email = clients.Email
+                }).ToList();
+                _displayClients = new ObservableCollection<ClientPresenter>(_clients);
+                FilterComboBox.ItemsSource = FilterValues;
+                SortComboBox.ItemsSource= SortValues;
+                ClientsListBox.ItemsSource = _displayClients;
+
+            }
+            
+        }
+    }
+}
